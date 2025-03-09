5f61b8360a5900ffba8e2f008c05934b807aefe8
杨紫晴 <yangziqing@MacBook-Pro.local>
2025-03-09 23:22:48 +0000

    Fix: Dynamic path for local and PythonAnywhere

--- professor_rating/wsgi.py
+++ professor_rating/wsgi.py
@@ -1,6 +1,16 @@
+
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+
+if 'PYTHONANYWHERE_DOMAIN' in os.environ:
+    path = '/home/sc22zy/3011-professor-rating-system'
+else:
+    path = os.path.dirname(os.path.abspath(__file__))
+
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'professor_rating.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
